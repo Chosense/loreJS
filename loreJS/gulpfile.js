2b596d77c9ac983ab8186ffb39adeafd8b512f6f
Mika Berglund <mika.berglund@develore.com>
2015-12-12 14:26:13 +0200

    Added minified versions of JS files.

--- loreJS/gulpfile.js
+++ loreJS/gulpfile.js
@@ -5,9 +5,15 @@ Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 */
 
 var gulp = require('gulp');
+var uglify = require("gulp-uglify");
+var rename = require("gulp-rename");
 
 gulp.task('default', function () {
     
+});
+
+gulp.task("clean", function () {
+
 });
 
 gulp.task("copy:js", function () {
@@ -21,4 +27,14 @@ gulp.task("copy:typeDefs", function () {
         .pipe(gulp.dest("./../dist"));
 });
 
-gulp.task("afterBuild", ["copy:typeDefs", "copy:js"]);
+gulp.task("min:js", function () {
+    gulp.src("./TypeScript/*.js")
+        .pipe(uglify())
+        .pipe(rename({
+            extname: ".min.js"
+        }))
+        .pipe(gulp.dest("./wwwroot/js/loreJS/"))
+        .pipe(gulp.dest("./../dist/"));
+});
+
+gulp.task("afterBuild", ["copy:typeDefs", "min:js", "copy:js"]);

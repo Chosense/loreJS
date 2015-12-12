d9659b9b6a64f34402a1da6b9f55e3207d692c3a
Mika Berglund <mika.berglund@develore.com>
2015-12-12 14:38:37 +0200

    Added a clean task and restructured distributed files.

--- loreJS/gulpfile.js
+++ loreJS/gulpfile.js
@@ -1,4 +1,4 @@
-﻿/// <binding AfterBuild='afterBuild' ProjectOpened='default' />
+﻿/// <binding AfterBuild='afterBuild' Clean='clean' ProjectOpened='default' />
 /*
 This file in the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
@@ -7,24 +7,27 @@ Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 var gulp = require('gulp');
 var uglify = require("gulp-uglify");
 var rename = require("gulp-rename");
+var rimraf = require("rimraf");
 
 gulp.task('default', function () {
     
 });
 
-gulp.task("clean", function () {
-
+gulp.task("clean:js", function (cb) {
+    rimraf("./../dist/**/*.js", cb);
+    rimraf("./../dist/**/*.ts", cb);
 });
+gulp.task("clean", ["clean:js"]);
 
 gulp.task("copy:js", function () {
     gulp.src("./TypeScript/*.js")
-        .pipe(gulp.dest("./../dist/"))
+        .pipe(gulp.dest("./../dist/js/"))
         .pipe(gulp.dest("./wwwroot/js/loreJS/"));
 });
 
 gulp.task("copy:typeDefs", function () {
     gulp.src("./TypeScript/*.d.ts")
-        .pipe(gulp.dest("./../dist"));
+        .pipe(gulp.dest("./../dist/js/"));
 });
 
 gulp.task("min:js", function () {
@@ -34,7 +37,7 @@ gulp.task("min:js", function () {
             extname: ".min.js"
         }))
         .pipe(gulp.dest("./wwwroot/js/loreJS/"))
-        .pipe(gulp.dest("./../dist/"));
+        .pipe(gulp.dest("./../dist/js/"));
 });
 
 gulp.task("afterBuild", ["copy:typeDefs", "min:js", "copy:js"]);
